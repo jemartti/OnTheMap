2a53e055167ffb6a53835277e004a095af36ca11
Jacob Marttinen <jacob@marttinen.ca>
2017-03-05 13:19:14 -0500

    Doing some initial scaffolding

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheMap
 //
 //  Created by Jacob Marttinen on 2/12/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
